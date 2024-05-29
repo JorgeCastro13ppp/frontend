b5b8f744963db3fc3bacc1446e116a1251dd494d
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-05-30 01:41:09 +0200

    modificar games page, hacer poker,ruleta,blackjact, login-register, apuestas

--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
@@ -10,6 +10,9 @@ export class GamesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init({
+      duration: 2000,
+    });
   }
 
 }

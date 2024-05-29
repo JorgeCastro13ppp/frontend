64269c4dba927fac09b06142457d5b129d78ccac
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-05-29 03:02:51 +0200

    estructura II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { UserProfileComponent } from './auth/user-profile/user-profile.component';
+import { BetsComponent } from './components/bets/bets.component';
+import { GamesComponent } from './components/games/games.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserProfileComponent,
+    BetsComponent,
+    GamesComponent,
+    NavbarComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

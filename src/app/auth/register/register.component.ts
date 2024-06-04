825169dff7f92c87bf1f6d805201a0f7ba1e491d
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-06-04 17:33:11 +0200

    terminar validaciones, ruleta,tragaperras,blackjack,poker y bet-page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from './_helpers';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!:FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['',Validators.email],
+      password: ['',[Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['',Validators.required],
+      acceptTerms: [false, Validators.requiredTrue]
+
+    },{
+      validators: MustMatch('password','confirmPassword')
+    });
   }
+
+  get f(){
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted=true;
+
+    if(this.registerForm.invalid){
+      return
+    }
+    alert('Éxito \n\n'+JSON.stringify(this.registerForm.value,null,4));
+  }
+
+  onReset(){
+    this.submitted = false;
+    this.registerForm.reset();
+  }
+
 
 }
--- src/app/components/slots/slots.component.ts
+++ src/app/components/slots/slots.component.ts
@@ -8,53 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SlotsComponent implements OnInit {
 
   constructor() { }
-
-  bet = 1;
-  reel1?:number;
-  reel2?:number;
-  reel3?:number;
-  winnings = 0;
-
   ngOnInit(): void {
-    this.resetReels();
-  }
-
-  play(): void {
-    this.resetReels();
-    this.generateReels();
-    this.checkWin();
-  }
-
-  increaseBet(): void {
-    this.bet++;
-  }
-
-  collect(): void {
-    this.winnings = 0;
-  }
-
-  resetReels(): void {
-    this.reel1 = 0;
-    this.reel2 = 0;
-    this.reel3 = 0;
-  }
-
-  generateReels(): void {
-    this.reel1 = this.getRandomNumber();
-    this.reel2 = this.getRandomNumber();
-    this.reel3 = this.getRandomNumber();
-  }
-
-  checkWin(): void {
-    if (this.reel1 === this.reel2 && this.reel2 === this.reel3) {
-      this.winnings = this.bet * 4;
-    } else if (this.reel1 === this.reel2 || this.reel2 === this.reel3 || this.reel1 === this.reel3) {
-      this.winnings = this.bet * 2;
-    }
-  }
-
-  getRandomNumber(): number {
-    return Math.floor(Math.random() * 10) + 1;
+    throw new Error('Method not implemented.');
   }
 
 

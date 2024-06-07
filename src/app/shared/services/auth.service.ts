dae579ab73c78d77c759f6efd91515e7d3331448
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-06-07 03:29:25 +0200

    laravel host

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -9,10 +9,10 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   constructor(private httpClient:HttpClient) { }
-  private apiUrl = 'http://127.0.0.1:8000/api';
+  private apiUrl = 'https://back-end-production-71ca.up.railway.app/api';
 
   getUser() {
-    return this.httpClient.get<User>(`http://127.0.0.1:8000/tests`);
+    return this.httpClient.get<User>(`https://back-end-production-71ca.up.railway.app/tests`);
   }
 
   register(userData: any): Observable<any> {

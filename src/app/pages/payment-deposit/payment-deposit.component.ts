f5085853ad755e6f1a6752f1d9e0661f8caabdb0
JorgeCastro13ppp <jlcastroc01@educarex.es>
2024-06-10 15:46:33 +0200

    alert en deposit y withdrawal, juegos

--- src/app/pages/payment-deposit/payment-deposit.component.ts
+++ src/app/pages/payment-deposit/payment-deposit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-deposit',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-deposit.component.scss']
 })
 export class PaymentDepositComponent implements OnInit {
+  depositForm:FormGroup = new FormGroup({});
 
-  constructor() { }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.depositForm = this.formBuilder.group({
+      nombreTitular: ['', [Validators.required, Validators.minLength(10)]],
+      numeroTarjeta: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]],
+      fechaCaducidad: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/(\d{2})$/)]],
+      cvv: ['', [Validators.required, Validators.pattern(/^\d{3}$/)]]
+    });
+  }
+
+  submitForm() {
+    if (this.depositForm.valid) {
+      // Realizar acción de depósito
+      console.log('Formulario válido, se puede proceder con el depósito');
+      alert('Se procederá con el depósito.')
+    } else {
+      // Mostrar errores o mensaje de que el formulario no es válido
+      console.log('Formulario inválido, no se puede proceder con el depósito');
+      alert('No se puede proceder con el depósito')
+    }
   }
 
 }
--- src/app/pages/payment-withdrawals/payment-withdrawals.component.ts
+++ src/app/pages/payment-withdrawals/payment-withdrawals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-withdrawals',
@@ -7,14 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentWithdrawalsComponent implements OnInit {
 
-  constructor() {
-    console.log('PaymentDepositComponent constructor called');
+  withDrawForm:FormGroup = new FormGroup({});
 
-   }
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-    console.log('PaymentDepositComponent ngOnInit called');
+    this.withDrawForm = this.formBuilder.group({
+      nombreTitular: ['', [Validators.required, Validators.minLength(10)]],
+      numeroTarjeta: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]],
+      fechaCaducidad: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/(\d{2})$/)]],
+      cvv: ['', [Validators.required, Validators.pattern(/^\d{3}$/)]]
+    });
+  }
 
+  submitForm() {
+    if (this.withDrawForm.valid) {
+      // Realizar acción de depósito
+      console.log('Formulario válido, se puede proceder con el retiro');
+      alert('Se procederá con el retiro.')
+    } else {
+      // Mostrar errores o mensaje de que el formulario no es válido
+      console.log('Formulario inválido, no se puede proceder con el retiro');
+      alert('No se puede proceder con el retiro')
+    }
   }
 
 }
